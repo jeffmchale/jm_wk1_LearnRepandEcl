2513b4d7f3bc65fd699d7b75e67966d09b50c610
Jeff McHale <jeffmchale94@gmail.com>
2019-09-30 00:21:22 -0700

    Linking HelloWorld.java to Github

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,29 @@
+
+//HEADER
+//Program Name: HelloWorld.java
+//Author: Jeff McHale
+//Class: CS260 Fall 2019
+//Date: 09/30/19
+//Description: Program prints out string, "Hello World!" to the terminal.
+
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args ) {
+		System.out.println("Hello World!"); 
+	    // Prints string "Hello World!" to terminal
+	}
+	
+	
+}
+
+
+
+/* Footer
+Expected Output:
+
+	Hello World!
+
+*/
